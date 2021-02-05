3b36c38dd36db86b66c8783db05888070d3fd98a
Alvaro230 <surfiera@gmail.com>
2021-02-05 15:54:38 +0100

    Rest Coche

---
+++ EjercicioSpringRestCoche/src/main/java/com/uem/restcoche/SpringRestCocheApplication.java
@@ -0,0 +1,13 @@
+package com.uem.restcoche;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestCocheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestCocheApplication.class, args);
+	}
+
+}
